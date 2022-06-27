be2fd6ce57fc959dd65329b9a7b4bbb69c40807b
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-06-27 20:23:15 -0300

    Configuración de pool de conexiones con c3p0

--- src/com/hotel/factory/CrearConexion.java
+++ src/com/hotel/factory/CrearConexion.java
@@ -3,24 +3,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * 
- */
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * @author ezequiel
  *
  */
 public class CrearConexion {
+	
+	private DataSource dataSource;
+	
+	public CrearConexion() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://172.17.0.3/hotel?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("alura");
+		pooledDataSource.setMaxPoolSize(10);
+		
+		this.dataSource = pooledDataSource;
+	}
 
 	/**
 	 * @param args
 	 * @throws SQLException
 	 */
 	public Connection crearConexion() throws SQLException {
-		System.out.println("Conectando base de datos...");
-		return DriverManager.getConnection("jdbc:mysql://172.17.0.3/hotel?useTimeZone=true&serverTimeZone=UTC", "root",
-				"alura");
+		return this.dataSource.getConnection();
+//		System.out.println("Conectando base de datos...");
+//		return DriverManager.getConnection("jdbc:mysql://172.17.0.3/hotel?useTimeZone=true&serverTimeZone=UTC", "root",
+//				"alura");
 
 	}
 
